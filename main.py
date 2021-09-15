4671f96a52c09f31e415c28ca68beff0526262f2
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-09-14 19:00:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(content_types=['text'])
 async def texthandler(m: types.Message):
-    if m.text != 'استلام البريد':
-        await m.answer(f'مرحبا, {m.from_user.mention}\nتم تصميم هذا الروبوت لتلقي رسائل  البريد المؤقت بسرعة. انقر فوق الزر أدناه', reply_markup=kb.menu)
-    elif m.text == 'استلام البريد':
+    if m.text != 'Receive mail':
+        await m.answer(f'welcome, {m.from_user.mention}\nThis bot is designed to receive temporary mail messages quickly.  Click the button below', reply_markup=kb.menu)
+    elif m.text == 'Receive mail':
         ma = Mailbox('')
         email = f'{ma._mailbox_}@1secmail.com'
-        await m.answer(f'➕ ها هو بريدك الإلكتروني: {email}\nارسل رسالة.\nيتم فحص البريد تلقائيًا ، كل 4 ثوانٍ ، إذا وصلت رسالة جديدة ، فسوف نخطرك!\nملاحظة: يمكنك تلقي فقط رسالة واحدة - لكل بريد إلكتروني  ')
+        await m.answer(f'➕ Here is your email: {email}\nSend a message.\nMail is checked automatically, every 4 seconds, if a new message arrives, we will notify you!\nNote: You can only receive one message - per email')
         while True:
             mb = ma.filtred_mail()
             if isinstance(mb, list):
@@ -27,7 +27,7 @@ async def texthandler(m: types.Message):
                 fromm = js['from']
                 theme = js['subject']
                 mes = js['textBody']
-                await m.answer(f'💬 رسالة جديدة:\n<b>من عند</b>: {fromm}\n<b>موضوع</b>: {theme}\n<b>رسالة</b>: {mes}', reply_markup=kb.menu, parse_mode='HTML')
+                await m.answer(f'💬 New message:\nfrom</b>: {fromm}\n<b>Issue</b>: {theme}\n<b>Message</b>: {mes}', reply_markup=kb.menu, parse_mode='HTML')
                 break
             else:
                 pass
